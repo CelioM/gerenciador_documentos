5b0d4f0ea634b860b56a604c5b89f7b0fb56386a
MathParamount <bellcruzsantos.com>
2025-06-09 15:49:15 -0300

    Foi feitas as mudanças no arquivo usuario.h

--- include/usuario.h
+++ include/usuario.h
@@ -3,40 +3,40 @@
 
 #include <iostream>
 #include <string>
+#include <vector>
 
 class Usuario
 {
 private:
-	std::string nome;
-	std::string status;
-	std::string comentarios;
-	std::string horario;
+	std::string nome_;
+	std::string comentarios_;
+	std::string senha_;
+	std::string tipo_;
 	
 public:
-	Usuario(const std::string& nome, const std::string& status, const std::string& comentarios, const std::string& horario) : nome(nome), status(status),comentarios(comentarios),horario(horario) {}
+	Usuario(const std::string& nome, const std::string& comentarios, const std::string& senha,const std::string& tipo) : nome_(nome), comentarios_(comentarios),senha_(senha), tipo_(tipo) {}
 
 	//Getters
 
-	std::string getNome() const {return nome;}
-	std::string getStatus() const {return status;}
-	std::string getComentarios() const {return comentarios;}
-	std::string getHorario() const {return horario;}
+	std::string getNome() const {return nome_;}
+	std::string getComentarios() const {return comentarios_;}
+	std::string getTipo() const {return tipo_;}
 
 	//Setters(definições)
 
-	void setNome(const std::string& n) {nome = n;}
-	void setStatus(const std::string& s) {status = s;}
-	void setComentarios(const std::string& c) {comentarios = c;}
-	void setHorario(const std::string h) {horario = h;}
+	void setNome(const std::string& n) {nome_ = n;}
+	void setComentarios(const std::string& c) {comentarios_ = c;}
+	void setSenha(const std::string s) {senha_ = s;}
+	void setTipo(const std::string& t) {tipo_ = t;}
 
-	//Exibição de informações
+	void status() const;
+	static bool login(const std::vector<Usuario>& usuarios, const std::string& nome,const std::string& senha);
+};
 
-	void exibir() const
-	{
-		std::cout << nome << "O status: " << status << "\n";
-		std::cout << "Comentarios: " << comentarios << "\n";
-		std::cout << " Horario: " << horario << std::endl;
-	}
+struct TipoUsuario
+{
+	std::vector<std::string> tipos;
+	Usuario usuario;
 };
 
 #endif
