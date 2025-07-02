50e671b881529f1b7a15eb728028e3d1d6d4b3f3
Gabriel da cruz guedes santos <154000020+MathParamount@users.noreply.github.com>
2025-07-02 11:17:12 -0300

    Atualizações no arquivo util.h
    Fiz algumas atualizações no arquivo util.h para a completude do arquivo util e para a eficiência do projeto

--- include/util.h
+++ include/util.h
@@ -1,13 +1,25 @@
-#ifndef UTIL_H
-#define UTIL_H
+#pragma once
 
 #include <string>
+#include <vector>
+#include <filesystem>
+
+namespace fs = std::filesystem;
 
 namespace Util {
-    std::string obterCaminhoDesktop();
+    fs::path  obterCaminhoDesktop();
     std::string obterDataHoraAtual();
     bool arquivoExiste(const std::string& caminho);
     void criarDiretorioSeNaoExistir(const std::string& caminho);
+	
+	bool limparDiretorio(const fs::path& caminho);
+	std::vector<std::string> limparArquivoNoDiretorio(const fs::path& caminho);
+
+	//Funções de validação
+	bool validarNomeDocumento(const std::string& nome);
+	bool validarRevisaoDocumento(const std::string& revisao);
+
+	std::string gerarNomeArquivoDocumento(const std::string& nomeDoc, const std::string& revisao);
+	std::string proximaRevisao(const std::string& revisaoAtual);
 }
 
-#endif
